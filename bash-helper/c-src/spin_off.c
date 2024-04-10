a469002350e37cbed2d12ba79be469c8b5c6a3f5
Florian Sorko <florian.begusch@gmail.com>
2024-04-10 10:09:26 +0000

    `bash-helper`: `spin_off.c`: rename process to pid

--- bash-helper/c-src/spin_off.c
+++ bash-helper/c-src/spin_off.c
@@ -4,11 +4,11 @@
 #include <string.h>
 
 void tmux_refresh_client() {
-    int process = fork();
-    if (process < 0) {
+    int pid = fork();
+    if (pid < 0) {
         printf("Fork error\n");
     }
-    if (process == 0) {
+    if (pid == 0) {
         char *exe_name = "tmux";
         char *second_param = "refresh-client";
         char *params[]  = {exe_name, second_param, NULL};
